f3db17a5a7b2adfe3df0dcea4d8c5dfdf5052a58
IiIymik <sambotemp@gmail.com>
2023-02-07 10:01:45 +0200

    add frontend

---
+++ frontend/next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+};
+
+module.exports = nextConfig;
